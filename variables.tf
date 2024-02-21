e3c34f63161d89d86a17e38f2778498961289be9
raghucode555 <raghavendrakv182@gmail.com>
2024-02-21 10:26:27 +0000

    all files commited

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "subnet_id_1" {
+  type = string
+  default = "subnet-01b8e3bf642e0aca"
+ }
+
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-0eec0b2194c4c231d"
+ }
+
+ variable "cluster_name" {
+  type = string
+  default = "my-eks-cluster"
+ }
